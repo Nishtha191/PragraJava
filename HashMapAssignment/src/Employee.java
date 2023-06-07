9990a4616f827847afb3f524a388ecc610f13406
Nishtha191 <patelnishtha009@gmail.com>
2023-06-06 21:21:23 -0400

    Initialize class

---
+++ HashMapAssignment/src/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    String firstName;
+    String lastName;
+    int age;
+    double salary;
+    String company;
+
+    public Employee(String firstName, String lastName, int age, double salary, String company) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+        this.company = company;
+    }
+}
---
+++ HashMapAssignment/src/Team.java
@@ -0,0 +1,13 @@
+public class Team {
+    String projectName;
+    String features;
+    String stakeholders;
+    double budget;
+
+    public Team(String projectName, String features, String stakeholders, double budget) {
+        this.projectName = projectName;
+        this.features = features;
+        this.stakeholders = stakeholders;
+        this.budget = budget;
+    }
+}
